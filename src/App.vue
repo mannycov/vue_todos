8c810ada515c1168942d861d450cf702bbd6a109
Emanuel <manny.cov09@gmail.com>
2020-12-03 21:41:35 -0800

    Add AddTodo component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Header />
+    <AddTodo />
     <Todos v-bind:todos="todos" v-on:delete-todo="deleteTodo" />
   </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data() {
     return {
@@ -49,5 +52,23 @@ export default {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form>
+      <input type="text" v-model="title" name="title" placeholder="Add todo item">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data () {
+    return {
+      title: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+    padding: 5px;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+
+</style>
