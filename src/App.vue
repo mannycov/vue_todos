414ec6d57fc8d3f3ee4f31e27b0d1fc1a8795463
Emanuel <manny.cov09@gmail.com>
2020-12-03 19:34:37 -0800

    Add todos component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    {{msg}}
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos'
 
 export default {
   name: 'App',
   components: {
+    Todos
   },
   data() {
     return {
