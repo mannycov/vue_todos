bbdde6df6fa340469346a9c64be786ae2d29f2ca
Emanuel <manny.cov09@gmail.com>
2020-12-03 19:10:09 -0800

    Add todos component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Vue Todo App</div>
+  <div id="app">
+    {{msg}}
+  </div>
 </template>
 
 <script>
@@ -7,17 +9,35 @@
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo One",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 </style>
