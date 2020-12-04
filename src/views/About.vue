87912bf08c4a25c48194ea87bd84674b29cf2bb3
Emanuel <manny.cov09@gmail.com>
2020-12-04 11:47:11 -0800

    Update About page

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About this Project</h1>
+    <p>Small Todo List project created with Vue on the frontend and using axios to make calls to a fake REST API called JSON Placeholder</p>
+  </div>
+</template>
